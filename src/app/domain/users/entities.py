e6bfadc8fb6e35d4c442f1c9ed1c127a3fd203eb
DenisHrtn <denispython16@gmail.com>
2025-02-20 16:30:08 +0300

    uof interface and specific class

---
+++ src/app/domain/users/entities.py
@@ -0,0 +1,12 @@
+from logging import getLogger
+
+
+loger = getLogger(__name__)
+
+
+class User:
+    """
+    User entity class and provides methods for creating and updating users
+    """
+
+    def __init__(self): pass
---
+++ src/app/infra/unit_of_work/async_sql.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+
+from src.app.app_layer.interfaces.unit_of_work.sql_base import IUnitOfWork
+
+
+class UnitOfWork(IUnitOfWork):
+    """
+    Provides a unit of work pattern for managing transactions and repositories in
+    an asynchronous SQLAlchemy session.
+    """
+
+    def __init__(self, session_factory: async_sessionmaker[AsyncSession]) -> None:
+        self._session_factory = session_factory
+
+    async def __aenter__(self) -> IUnitOfWork:
+        self._session = self._session_factory()
+
+        return await super().__aenter__()
+
+    async def commit(self) -> None:
+        await self._session.commit()
+
+    async def rollback(self) -> None:
+        await self._session.rollback()
+
+    async def close(self) -> None:
+        await self._session.close()
